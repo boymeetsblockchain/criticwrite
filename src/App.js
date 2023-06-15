9fd7aa991bebafd1b12945b4b1b084003f584f24
boymeetsblockchain <ajeoluwasegun18@gmail.com>
2023-06-15 08:19:17 +0100

    change the footer

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import {Route,Routes} from 'react-router-dom'
-import Auth from './pages/Auth'
 import Homepage from './pages/Homepage'
 import Payment from './pages/Payments'
 import Footer from './components/Footer'
+import Form from './pages/Form'
 
 function App() {
   return (
-     <div className='max-w-[100%] min-h-[100vh] bg-[#2a292b] text-[#e6dcc6]'>
+     <div className='max-w-[100%] min-h-[100vh] px-4 md:px-16 bg-[#2a292b] text-[#e6dcc6]'>
       <Routes>
         <Route path='/' element={<Homepage/>}/>
-        <Route path='/auth' element={<Auth/>}/>
+        <Route path='/form' element={<Form/>}/>
         <Route path='/Payment' element={<Payment/>}/>
       </Routes>
       <Footer/>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,25 +1,74 @@
 import React from 'react';
-import img from '../assets/img2.jpg';
+import {AiOutlineMail,AiFillFacebook,AiFillInstagram,AiOutlineTwitter} from "react-icons/ai"
+
 
 const Footer= ()=> {
 	return(
-	<footer className='flex flex-col md:flex-row justify-center align-center gap-3 mt-[30px]'>
-	<section className='m-[2%] p-[2%] w-[100%]'>
-		<div className='mb-[15px]'>
-			<h1 className='text-[36px] font-serif'>Get in touch!</h1>
-			<p className='mb-[9px]'>Find out how our products can help you be more productive.</p>
-			<button className='text-[16px] text-[#1f1e20] rounded-[25px] min-w-[190px] p-3 bg-[#e6dcc6]'>CONTACT US</button>
-		</div>
-		<div>
-			<p><span className='font-bold text-[16px]'>PHONE</span> (123) 456-7890</p>
-			<p><span className='font-bold text-[16px]'>EMAIL</span> hello@reallygreatsite.com</p>
-			<p><span className='font-bold text-[16px]'>SOCIAL</span></p>
-		</div>
-	</section>
-	<section className='min-h-[200px] w-[100%] bg-[grey]mb-[20px] max-h-[400px]'>
-	<img src={img} alt='' className='max-h-[100%] w-100%'/>
-	</section>
-	</footer>
+	 <footer className='flex  flex-col-reverse py-4 md:flex-row px-4 md:px-10  text-center justify-center'>
+      <div className="footer-icons flex flex-col flex-1 space-y-2 ">
+         <h1 className='text-3xl font-bold text-center mb-2'>CritiWrite</h1>
+		  <div className="icons flex  flex-col items-center">
+		  <h2 className='capitalize text-2xl'>follow us on our socials:</h2>
+			 <div className="icons-link flex justify-between space-x-2">
+			 <a href=""><AiFillInstagram size={30}/></a>
+			<a href=""><AiFillFacebook size={30}/></a>
+			<a href=""><AiOutlineTwitter size={30}/></a>
+			 </div>
+		  </div>
+		   <div className="address">
+		   <h2 className='font-bold mb-4'>Address:</h2>
+		   <p>1234 Street Name</p>
+		   <p>City, AA 99999</p>
+		   </div>
+		   <div className="contact">
+		   <h2 className='font-bold mb-4'>Contact:</h2>
+		   <p>Email: support@mobirise.com</p>
+		   <p>Phone: +1 (0) 000 0000 001</p>
+		    <p>Fax: +1 (0) 000 0000 002</p>
+		   </div>
+	  </div>
+	  <div className="footer-form flex-1">
+      <form className="max-w-md mx-auto p-4 bg-transparent rounded shadow-md">
+     
+	  <input
+        type="text"
+        id="text"
+        className="w-full px-2 py-5 mb-4  rounded-md border-none border-b-2 focus::outline-none active:outline-none"
+        required
+		placeholder='Name'
+      />
+      <input
+        type="email"
+        id="email"
+        className="w-full px-2 py-5 mb-4  rounded-md border-none border-b-2 focus::outline-none active:outline-none"
+        required
+		placeholder='Email'
+      />
+
+     
+      <input
+        type="tel"
+        id="phone"
+		className="w-full px-2 py-5 mb-4  rounded-md border-none border-b-2 focus::outline-none active:outline-none"
+        required
+		 placeholder='phone number'
+      />
+
+      <textarea
+        id="message"
+        className="w-full px-3 py-7 mb-4 border rounded-md"
+        required
+      ></textarea>
+
+      <button
+        type="submit"
+        className="w-full px-4 py-2 font-semibold text-white min-w-max bg-transparent"
+      >
+        Submit
+      </button>
+    </form>
+	  </div>
+	 </footer>
 	)
 }
 

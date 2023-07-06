6c7f469d6e10d192e06c9bf749f1b483c862ce94
3illBaby <thrillanon@outlook.com>
2023-07-06 23:31:59 +0100

    minor updates

--- src/pages/Form.jsx
+++ src/pages/Form.jsx
@@ -1,122 +1,50 @@
-import React, { useRef } from 'react';
-import emailjs from '@emailjs/browser';
-import toast from 'react-hot-toast'
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
-import { tag } from '../assets/index.js';
+import { tag } from "../assets/index.js";
 
-const Form = ({updateName, updateEmail, updatePhone}) => {
+const Form = ({ updateName, updateEmail, updatePhone }) => {
   const form = useRef();
   const navigate = useNavigate();
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('service_b9rb4do', 'template_xocrpdo', form.current, 'cNr8CRb1dMidHfvjQ')
-      .then((result) => {
+    emailjs
+      .sendForm(
+        "service_b9rb4do",
+        "template_xocrpdo",
+        form.current,
+        "cNr8CRb1dMidHfvjQ"
+      )
+      .then(
+        (result) => {
           console.log(result.text);
-          toast.success("essay sent succesfully")
-          navigate('/payment')
-      }, (error) => {
+          toast.success("essay sent succesfully");
+          navigate("/payment");
+        },
+        (error) => {
           console.log(error.text);
-      });
+        }
+      );
 
-      e.target.reset()
+    e.target.reset();
   };
 
   return (
     <div className="py-2 space-y-2">
-      <h1 className="text-center text-[#167396] font-[serif] font-bold text-3xl md:text-4xl">Write an Essay</h1>
+      <h1 className="text-center gradient-text font-inter font-bold text-3xl md:text-5xl">
+        Write an Essay
+      </h1>
       <form
-      ref={form}
+        ref={form}
         onSubmit={sendEmail}
         className="flex flex-col space-y-2 px-4 items-center w-full py-4 justify-center md:px-8"
       >
-            <div className="relative">
-    <input 
-    name='name'
-    className="
-    block
-    rounded-md
-    px-6
-    pt-6
-    pb-1
-    w-[350px]
-    md:w-[500px]
-    text-md
-  text-black
-  bg-[#d1dbde]
-    appearance-none
-    focus:outline-none
-    focus:ring-0
-    peer
-    invalid:border-b-1
-    "
-    onChange={(e)=>updateName(e.target.value)}
-    required
-    placeholder=" " />
-<label  className="
-    absolute 
-    text-md
-  text-zinc-400
-    duration-150 
-    transform 
-    -translate-y-3 
-    scale-75 
-    top-4 
-    z-10 
-    origin-[0] 
-    left-6
-    peer-placeholder-shown:scale-100 
-    peer-placeholder-shown:translate-y-0 
-    peer-focus:scale-75
-    peer-focus:-translate-y-3
-">Full Name</label>
-</div>
-<div className="relative">
-    <input 
-    name='phone'
-    className="
-    block
-    rounded-md
-    px-6
-    pt-6
-    pb-1
-    w-[350px]
-    md:w-[500px]
-    text-md
-  text-black
-  bg-[#d1dbde]
-    appearance-none
-    focus:outline-none
-    focus:ring-0
-    peer
-    invalid:border-b-1
-    "
-    onChange={(e)=>updatePhone(e.target.value)}
-    type='number'
-    required
-    placeholder=" " />
-<label  className="
-    absolute 
-    text-md
-  text-zinc-400
-    duration-150 
-    transform 
-    -translate-y-3 
-    scale-75 
-    top-4 
-    z-10 
-    origin-[0] 
-    left-6
-    peer-placeholder-shown:scale-100 
-    peer-placeholder-shown:translate-y-0 
-    peer-focus:scale-75
-    peer-focus:-translate-y-3
-">Phone Number</label>
-</div>
         <div className="relative">
-    <input 
-     name='email'
-    className="
+          <input
+            name="name"
+            className="
     block
     rounded-md
     px-6
@@ -133,10 +61,12 @@ const Form = ({updateName, updateEmail, updatePhone}) => {
     peer
     invalid:border-b-1
     "
-    onChange={(e)=>updateEmail(e.target.value)}
-    required
-    placeholder=" " />
-<label htmlFor={"email"} className="
+            onChange={(e) => updateName(e.target.value)}
+            required
+            placeholder=" "
+          />
+          <label
+            className="
     absolute 
     text-md
   text-zinc-400
@@ -152,15 +82,112 @@ const Form = ({updateName, updateEmail, updatePhone}) => {
     peer-placeholder-shown:translate-y-0 
     peer-focus:scale-75
     peer-focus:-translate-y-3
-">Email</label>
-</div>
+"
+          >
+            Full Name
+          </label>
+        </div>
+        <div className="relative">
+          <input
+            name="phone"
+            className="
+    block
+    rounded-md
+    px-6
+    pt-6
+    pb-1
+    w-[350px]
+    md:w-[500px]
+    text-md
+  text-black
+  bg-[#d1dbde]
+    appearance-none
+    focus:outline-none
+    focus:ring-0
+    peer
+    invalid:border-b-1
+    "
+            onChange={(e) => updatePhone(e.target.value)}
+            type="number"
+            required
+            placeholder=" "
+          />
+          <label
+            className="
+    absolute 
+    text-md
+  text-zinc-400
+    duration-150 
+    transform 
+    -translate-y-3 
+    scale-75 
+    top-4 
+    z-10 
+    origin-[0] 
+    left-6
+    peer-placeholder-shown:scale-100 
+    peer-placeholder-shown:translate-y-0 
+    peer-focus:scale-75
+    peer-focus:-translate-y-3
+"
+          >
+            Phone Number
+          </label>
+        </div>
+        <div className="relative">
+          <input
+            name="email"
+            className="
+    block
+    rounded-md
+    px-6
+    pt-6
+    pb-1
+    w-[350px]
+    md:w-[500px]
+    text-md
+  text-black
+  bg-[#d1dbde]
+    appearance-none
+    focus:outline-none
+    focus:ring-0
+    peer
+    invalid:border-b-1
+    "
+            onChange={(e) => updateEmail(e.target.value)}
+            required
+            placeholder=" "
+          />
+          <label
+            htmlFor={"email"}
+            className="
+    absolute 
+    text-md
+  text-zinc-400
+    duration-150 
+    transform 
+    -translate-y-3 
+    scale-75 
+    top-4 
+    z-10 
+    origin-[0] 
+    left-6
+    peer-placeholder-shown:scale-100 
+    peer-placeholder-shown:translate-y-0 
+    peer-focus:scale-75
+    peer-focus:-translate-y-3
+"
+          >
+            Email
+          </label>
+        </div>
         <div className="relative">
           <textarea
             id="essay"
-            name='essay'
+            name="essay"
             className="block rounded-md px-6 pt-6 pb-1 w-[350px] md:w-[500px] 
              h-[280px] text-md text-[black] bg-[#d1dbde] appearance-none focus:outline-none focus:ring-0 peer invalid:border-b-1"
-             required
+            required
           ></textarea>
           <label
             htmlFor="essay"
@@ -170,11 +197,15 @@ const Form = ({updateName, updateEmail, updatePhone}) => {
             Write your Essay Here
           </label>
         </div>
-        <div className='flex'>
-        <img src={tag} alt='' height='100px' width='100px' className=''/>
-        <button type='submit'
-         className="text-[16px] hover:bg-[#167396] text-[#1f1e20] rounded-[25px] min-w-[170px] p-3 bg-[#d1dbde]">Submit</button>
-         </div>
+        <div className="flex">
+          <img src={tag} alt="" height="100px" width="100px" className="" />
+          <button
+            type="submit"
+            className="text-[16px] hover:bg-[#167396] font-work font-semibold text-[#1f1e20] rounded-[25px] min-w-[170px] p-3 bg-[#d1dbde]"
+          >
+            Submit
+          </button>
+        </div>
       </form>
     </div>
   );

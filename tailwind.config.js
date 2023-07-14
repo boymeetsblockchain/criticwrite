6503fdf52fd92fcf522b87d6360ecadddc5ee84f
3illBaby <thrillanon@outlook.com>
2023-07-15 00:15:12 +0100

    added new font face

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [".index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  content: ['.index.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
     colors: {
-      primary: "#2A292B",
-      secondary: "#E6DCC6",
+      primary: '#2A292B',
+      secondary: '#E6DCC6',
     },
     fontFamily: {
-      work: ["Work Sans", "sans-serif"],
-      montserrat: ["Montserrat", "sans-serif"],
-      sans: ["ui-sans-serif", "system-ui"],
-      serif: ["ui-serif", "Georgia"],
-      inter: ["Inter", "sans-serif"],
+      work: ['Work Sans', 'sans-serif'],
+      montserrat: ['Montserrat', 'sans-serif'],
+      sans: ['ui-sans-serif', 'system-ui'],
+      serif: ['ui-serif', 'Georgia'],
+      inter: ['Inter', 'sans-serif'],
+      lexend: ['Lexend Deca', 'sans-serf'],
     },
   },
   plugins: [],
